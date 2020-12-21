f6fe7a863eda47bfb8a0688691540137e50dba53
Marko <marko.nikolas@protonmail.com>
2020-12-21 23:30:22 +0100

    added editor config

--- src/scripts/class/Greeting.js
+++ src/scripts/class/Greeting.js
@@ -1,19 +1,19 @@
 export class Greeting {
-    constructor(message) {
-        this._message = message;
-    }
+	constructor(message) {
+		this._message = message;
+	}
 
-    get message() {
-        return this._message;
-    }
+	get message() {
+		return this._message;
+	}
 
-    greet() {
-        return this.message;
-    }
+	greet() {
+		return this.message;
+	}
 }
 
 export class Bye {
-    bye() {
-        console.log('Goodbye!');
-    }
+	bye() {
+		console.log("Goodbye!");
+	}
 }
