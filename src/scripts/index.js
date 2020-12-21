50d90e213c369eb45ebd8b40bb421d1172aca942
Marko <marko.nikolas@protonmail.com>
2020-12-21 21:40:55 +0100

    cleaned assets

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,3 @@
-import { utils } from './utils/util';
 import { Greeting } from './class/Greeting';
-
 const title = new Greeting('Hello World');
-
 document.querySelector('.starter__title').innerHTML = title.greet() + ' !';
-
-utils();
