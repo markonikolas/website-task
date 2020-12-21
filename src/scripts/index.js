946f00545d34208c6e54cca4a763dd04dcddadcf
Marko <marko.nikolas@protonmail.com>
2020-12-21 12:18:53 +0100

    modified utils.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,4 @@
+import './utils/util';
 import { Greeting } from './class/Greeting';
-import { cube } from './utils/math';
 
-const Hello = new Greeting('Hello World');
-Hello.greet();
-
-function component() {
-    const element = document.createElement('pre');
-    element.innerHTML = ['Hello webpack!', '5 cubed is equal to ' + cube(5)].join('\n\n');
-    return element;
-}
-
-document.body.appendChild(component());
-
-console.log(123);
+new Greeting('Hello World').greet();
