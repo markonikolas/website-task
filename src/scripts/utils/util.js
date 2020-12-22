838af5649d34261720b95f893f1f1adf1a03b805
Marko <marko.nikolas@protonmail.com>
2020-12-22 18:46:22 +0100

    added splitting for js

--- src/scripts/utils/util.js
+++ src/scripts/utils/util.js
@@ -1,5 +1,9 @@
 // Utility file, import if neded.
+import { join, add } from 'lodash-es';
 
 export default function utils() {
-	return `Utilities loaded.`;
+	const arr = [ 1, 2, 5, 6, 7 ];
+	const addition = add( arr[1] + arr[2], arr[3] );
+
+	return join( arr, addition, ' ' );
 }
